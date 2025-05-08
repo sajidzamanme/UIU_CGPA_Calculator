cc567fe05ae9284e84ebd255566682d111a3fb2d
Sajid <sajidzaman.2002.3@gmail.com>
2025-05-09 01:04:24 +0600

    update state management and add modal for CGPA summary

---
+++ src/components/Modal.jsx
@@ -0,0 +1,47 @@
+const Modal = ({ currentStatus, semesterStatus, setIsModalVisible }) => {
+  const closeModal = () => {
+    setIsModalVisible((prevState) => !prevState);
+  };
+
+  const totalCredit =
+    Number(currentStatus.currentCredits) +
+    Number(semesterStatus.semesterCredits);
+  const CGPA =
+    totalCredit > 0
+      ? (
+          (Number(semesterStatus.semesterCredits) *
+            Number(semesterStatus.semesterGPA) +
+            Number(currentStatus.currentCGPA) *
+              Number(currentStatus.currentCredits)) /
+          totalCredit
+        ).toFixed(2)
+      : 0;
+
+  return (
+    <div className="absolute top-0 left-0 flex flex-row justify-center items-center h-dvh w-full z-50">
+      <div
+        onClick={closeModal}
+        className="fixed z-0 bg-black opacity-60 h-full w-full"
+      ></div>
+      <div className="z-10 flex flex-col gap-2 bg-white p-6 rounded-lg shadow-lg w-3/4">
+        <h1 className="text-center text-2xl font-semibold text-orange-500">
+          Summary:
+        </h1>
+        <div className="flex flex-col">
+          <h1 className="text-orange-500 font-medium">
+            Total Semester Credits: {semesterStatus.semesterCredits}
+          </h1>
+          <h1 className="text-orange-500 font-medium">
+            Semester GPA: {semesterStatus.semesterGPA}
+          </h1>
+          <h1 className="text-orange-500 font-medium">
+            Total Completed Credits: {totalCredit}
+          </h1>
+          <h1 className="text-orange-500 font-medium">CGPA: {CGPA}</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
